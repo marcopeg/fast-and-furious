b0d8bd23de9cf2fc461ad7da4ee27a9daca2666f
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-03-31 10:18:04 +0200

    add handle query params capabilities (just basic implementation)

--- api/controllers/CarsController.js
+++ api/controllers/CarsController.js
@@ -5,7 +5,23 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var _findOne = require('../services/find-one');
+
 module.exports = {
-	
+	findOne: findOne,
 };
 
+function findOne(req, res) {
+	Cars
+		.find(_findOne.makePayload(req, res))
+		.then(_findOne.handleResults(req, res))
+		.then(handleQueryFields(req, res))
+		.then(_findOne.sendResults(req, res))
+		.catch(err => res.serverError(err));
+}
+
+function handleQueryFields(req, res) {
+    return () => {
+        console.log('QUERY', req.query);
+    };
+}
